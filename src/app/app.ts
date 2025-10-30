693aaa38ca4df28c696341900a5bb8c640d1a6be
saha-7op <saha7pritam@gmail.com>
2025-10-30 18:51:17 +0530

    Header Style

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Home],
   template: `
-    <h1>Welcome to {{ title() }}!</h1>
-
-    <router-outlet />
+    <app-header/>
+    <app-home/>
   `,
   styles: [],
 })
